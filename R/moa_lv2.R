e36f75e863b74cc633912e2c53665a4a9208f3e0
NayeongSon93 <nyson@drugsafe.or.kr>
2022-04-27 10:37:51 +0900

    commit

--- R/moa_lv2.R
+++ R/moa_lv2.R
@@ -27,6 +27,10 @@ moa_lv2<-function(input_schema=NULL, input_table=NULL, input_contents=NULL, inpu
 
   tmp_table<-tryCatch({dbGetQuery(con, render(sql, A=myschemaname, B=input_table, C=s$C, D=concept_id, E=s$E))},
                       error=function(e){showNotification(paste0(e[1]), type='err')})
+  if(nrow(tmp_table)==0){
+    table_lv2<-as.data.frame(list('Result'=c('No result: 0 Count')))
+    plot_lv2<-ggplot(table_lv2, aes(x=0, y=0, label='No result: 0 Count'))+geom_point(color='white')+theme_void()+geom_label()
+  } else {
 
   if(plot_id_s==1){
     table_lv2<-aggregate(Count~1+Gender, tmp_table, sum)%>%mutate(Prop=round(prop.table(Count)*100, 1))
@@ -59,10 +63,14 @@ moa_lv2<-function(input_schema=NULL, input_table=NULL, input_contents=NULL, inpu
 
   if(plot_id_s==5){
     tmp_table<-tmp_table%>%mutate('Visit_date'=as.Date(paste0(Visit_year, '-', Visit_month, '-', '1')))
-    table_lv2<-aggregate(Count~1+Visit_date+Visit_type, tmp_table, sum)
+    tmp_table<-tmp_table%>%mutate('Age'=Visit_year-Birth_year+1)
+    tmp_table<-tmp_table%>%mutate('Age_group'=cut(Age, breaks = c(seq(0, 90, by=10), Inf),
+                                                  label=c('0~9', '10~19', '20~29', '30~39', '40~49',
+                                                          '50~59', '60~69', '70~79', '80~89', '90+'), right=FALSE))
+    table_lv2<-aggregate(Count~1+Visit_date+Age_group, tmp_table, sum)
     plot_lv2<-
       ggplot(table_lv2) +
-      geom_line(aes(x=Visit_date, y=Count, group=factor(Visit_type), color=factor(Visit_type)),stat="identity")+
+      geom_bar(aes(x=Visit_date, y=Count, group=factor(Age_group), fill=factor(Age_group)), position='stack', stat="identity")+
       scale_x_date(date_breaks = "3 months", date_labels = "%b %Y")+
       theme(axis.text.x = element_text(angle=45, hjust=1))
   }
@@ -81,8 +89,10 @@ moa_lv2<-function(input_schema=NULL, input_table=NULL, input_contents=NULL, inpu
     table_lv2<-aggregate(Id~1+Visit_type+Visit_length, tmp_table, length); names(table_lv2)[3]<-'Count'
     table_lv2<-table_lv2%>%mutate('Visit_length_bin'=cut(Visit_length, breaks = c(0,7,30, 90, 180, Inf),
                                                    label=c('[0, 7)', '[7, 30)', '[30, 90)', '[90, 180)', '[180+)'), right=FALSE))
+    table_lv2<-aggregate(Count~Visit_type+Visit_length_bin, table_lv2, sum)
     plot_lv2<-table_lv2%>%ggplot(aes(x=Visit_length_bin, y=Count, group=1))+ geom_bar(stat = 'identity')+
-              scale_y_continuous(labels = function(x) format(x, scientific = FALSE))+facet_wrap(~Visit_type)
+              scale_y_continuous(labels = function(x) format(x, scientific = FALSE))+facet_wrap(~Visit_type)+
+              scale_x_discrete(name='length of visit')
   }
 
   if(plot_id_s==8){
@@ -190,12 +200,42 @@ moa_lv2<-function(input_schema=NULL, input_table=NULL, input_contents=NULL, inpu
                           scale_x_continuous(name='Number of concepts, per person')
   }
 
-  if(plot_id_s>20){
+
+  if(plot_id_s%in%c(21:24)){
+    tmp_table<-tmp_table%>%mutate('Data_date'=as.Date(paste0(Data_year, '-', Data_month, '-', '01')))
+    table_lv2<-aggregate(Count~Data_date, tmp_table, sum)
+    plot_lv2<-table_lv2%>%ggplot(aes(x=Data_date, y=Count))+geom_line()+
+      scale_x_date(date_breaks = "3 months", date_labels = "%b %Y")+
+      theme(axis.text.x = element_text(angle=45, hjust=1))
+  }
+
+  if(plot_id_s%in%c(25:28)){
+    tmp_table<-tmp_table%>%mutate('Data_date'=as.Date(paste0(Data_year, '-', Data_month, '-', '01')))
+    table_lv2<-aggregate(Count~Data_date+Gender, tmp_table, sum)
+    plot_lv2<-ggplot(table_lv2)+geom_line(aes(x=Data_date, y=Count, group=factor(Gender), color=factor(Gender)))+
+      scale_x_date(date_breaks = "3 months", date_labels = "%b %Y")+
+      theme(axis.text.x = element_text(angle=45, hjust=1))+
+      scale_color_discrete(name='Gender')
+  }
+
+  if(plot_id_s%in%c(29:32)){
+    tmp_table<-tmp_table%>%mutate('Data_date'=as.Date(paste0(Data_year, '-', Data_month, '-', '01')), 'Age'=Data_year-Byear+1)
+    tmp_table<-tmp_table%>%mutate('Age_group'=cut(Age, breaks = c(seq(0, 90, by=10), Inf),
+                                                  label=c('0~9', '10~19', '20~29', '30~39', '40~49','50~59', '60~69', '70~79', '80~89', '90+'), right=FALSE))
+    table_lv2<-aggregate(Count~Data_date+Age_group, tmp_table, sum)
+    plot_lv2<-ggplot(table_lv2)+geom_bar(aes(x=Data_date, y=Count, fill=factor(Age_group)), position='stack', stat='identity')+
+      scale_x_date(date_breaks = "3 months", date_labels = "%b %Y")+
+      theme(axis.text.x = element_text(angle=45, hjust=1))+
+      scale_fill_discrete(name='Age group')
+  }
+
+  if(plot_id_s>32){
     table_lv2<-as.data.frame(NULL)
     plot_lv2<-NULL
   }
 
-  disconnect(con)
+  }
 
+  disconnect(con)
   return(list('table_lv2'=table_lv2, 'plot_lv2'=plot_lv2))
 }
