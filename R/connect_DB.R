7af6e1046c7bc2fdb2787a2843d7dcb229ec4379
KIDS_PE <99954405+KIDS-PE@users.noreply.github.com>
2022-05-10 17:16:22 +0900

    allow NULL in connection details

--- R/connect_DB.R
+++ R/connect_DB.R
@@ -14,10 +14,13 @@ connect_DB<-function(){
   con_info<-readRDS(file.path(system.file(package="moadqproject"), 'results/con_info.rds'))
 
   if(is.null(con_info$password)==TRUE){con_info$password<-''}
+  if(is.null(con_info$host)==TRUE){local_server<-con_info$dbname}
+  if(is.null(con_info$dbname)==TRUE){local_server<-con_info$host}
+  if(is.null(con_info$host)==FALSE & is.null(con_info$dbname)==FALSE){local_server<-paste0(con_info$host, '/', con_info$dbname)}
 
   Sys.setenv("DATABASECONNECTOR_JAR_FOLDER" = con_info$jdbcDrivers)
   connectionDetails <- createConnectionDetails(dbms=con_info$dbtype,
-                                               server=paste0(con_info$host, '/', con_info$dbname),
+                                               server=local_server,
                                                port=con_info$port,
                                                user=con_info$user,
                                                password=con_info$password)
